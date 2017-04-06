f435d3eb52b29ee322edb3fad4e67aab87eed078
nemo <vzabudko@gmail.com>
2017-04-06 12:12:13 +0300

    ez

--- js/main.js
+++ js/main.js
@@ -234,7 +234,7 @@ $(function() {
             $top -= 100;
         $("html, body").stop().animate({
             scrollTop: $top
-        }, 1600, "easeInOutExpo");
+        }, 1500, "easeInOutExpo");
         event.preventDefault()
     })
 });
